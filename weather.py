b83b5dfd419dbde3961787067db4c33163a2ac6d
Aymane Boumaaza <aymaneboumaaza@gmail.com>
2020-06-18 17:46:33 +0100

    added args

--- weather.py
+++ weather.py
@@ -5,50 +5,80 @@ import json
 import urllib.request
 import sys
 import pprint
-from configparser import ConfigParser,NoOptionError
+from configparser import ConfigParser,NoSectionError
+import argparse
+import colorama
+from colorama import Fore, Style
 
 def save_config():
     city = input("City Name : ")
-    lang = input("Language : ")
-    units = input("Units system (metric or imperial):")
-    save = input("Save config ? [Y/N] ")
-    if save.upper() == 'Y':
-        config = ConfigParser()
-        config.read('config.ini')
-        config.add_section('main')
-        config.set('main','city',city)
-        config.set('main','units',units)
-        config.set('main','lang',lang)
-        config.set('main','key',key)
-        with open('config.ini', 'w') as f:
-            config.write(f)
-    fetch_and_display(city,units,lang,key)
+    units = input("Units system metric or imperial [m/i]: ")
+    while True:
+        if units.lower() not in ['m','i'] :
+            units = input('Wrong input. Units system (metric or imperial): ')
+            continue
+        else:
+            break
+    config = ConfigParser()
+    config.read('config.ini')
+    config.remove_section('MAIN')
+    config.add_section('MAIN')
+    config.set('MAIN','city',city)
+    config.set('MAIN','units',units)
+    config.set('MAIN','key',key)
+    with open('config.ini', 'w') as f:
+        config.write(f)
+    fetch_and_display(city,units,key)
 
 def load_config():
     try:
         config = ConfigParser()
         config.read('config.ini')
-        city = config.get('main','city')
-        units = config.get('main','units')
-        lang = config.get('main','lang')
-        key = config.get('main','key')
-        fetch_and_display(city,units,lang,key)
+        city = config.get('MAIN','city')
+        units = config.get('MAIN','units')
+        key = config.get('MAIN','key')
+        fetch_and_display(city,units,key)
 
     except AttributeError as missing_data:
-        print('Missing data in config file!', missing_data)
+        while True:
+            save = input('Missing data in config file! Create config now [Y/N] ? ').upper()
+            if save not in ['Y','N'] :
+                save = input('Wrong input. Create config now [Y/N] ? ').upper()
+                continue
+            else:
+                if save == 'Y':
+                    save_config()
+                else:
+                    exit(1)
 
     except urllib.error.HTTPError as apiError:
         print('Error: Missing or Unauthorized API!', apiError)
+        exit(1)
 
     except IOError as e:
         print('Error: No internet! ', e)
+        exit(1)
 
-    except NoOptionError as fi:
-        print('Configuration file is missing or corrupted! \nTo fix it, use --c or --config to fix it.')
+    except NoSectionError as fi:
+        while True:
+            save = input('Configuration file is missing or corrupted! Create config now [Y/N] ? ').upper()
+            if save not in ['Y','N'] :
+                save = input('Wrong input. Create config now [Y/N] ? ').upper()
+                continue
+            else:
+                if save == 'Y':
+                    save_config()
+                    break
+                else:
+                    exit(1)
 
-def build_url(city,unit,lang,key):
+def build_url(city,unit,key):
     api_url = 'http://api.openweathermap.org/data/2.5/weather?q='
-    full_url = api_url + city + '&mode=json&units=' + unit + '&APPID=' + key
+    if unit =='m':
+        units = 'metric'
+    else:
+        units = 'imperial'
+    full_url = api_url + city + '&mode=json&units=' + units + '&APPID=' + key
     return full_url
 
 def fetch_data(full_url):
@@ -65,12 +95,17 @@ def convert_time(time,format):
     else:
         return datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d')
 
-def build_dict(data):
+def build_dict(data,units):
     sys = data.get('sys')
     main = data.get('main')
     weather = data.get('weather')
     coord = data.get('coord'),
+    if units == 'm':
+        unit = "°C"
+    else :
+        unit = "°F"
     final_data = dict(
+    unit = unit,
     dt = convert_time(data.get('dt'),'h'),
     day = convert_time(data.get('dt'),'d'),
     city_id = data.get('id'),
@@ -91,7 +126,7 @@ def build_dict(data):
 def display(data,city):
     print('---------------------------------------')
     print(f"Current weather in: {city.capitalize()}, {data['country']} on {data['day']} :")
-    print(f"{data['temp']}°C - {data['weather_desc']}")
+    print(f"{data['temp']}{data['unit']} - {data['weather_desc']}")
     print(f"Max: {data['temp_max']}, Min: {data['temp_min']}")
     print('')
     print(f"Cloud: {data['clouds']}%")
@@ -101,26 +136,20 @@ def display(data,city):
     print(f"Last update from the server: {data['dt']}")
     print('---------------------------------------')
 
-def fetch_and_display(city,units,lang,key):
-    url = build_url(city,units,lang,key)
+def fetch_and_display(city,units,key):
+    url = build_url(city,units,key)
     data = fetch_data(url)
-    final_data = build_dict(data)
+    final_data = build_dict(data,units)
     display(final_data,city)
 
 def args():
-    args = sys.argv
-    args.pop(0)
-    if len(args) == 0:
+    parser = argparse.ArgumentParser(description = "A weather program fetching data from openweathermap API")
+    parser.add_argument('-c', '--config', action = 'store_true', help = 'Create config file (city - units - API key)')
+    args = parser.parse_args()
+    if args.config == True:
+        save_config()
+    else :
         load_config()
-    else:
-        if args[0] == '--config' or args[0] == '--c':
-            save_config()
-        elif args[0] == '--help' or args[0] == '--h':
-            print(__doc__)
-            exit()
-        else:
-            print("Unknown Command, available command: --help and --config")
-            exit(1)
 
 def main():
     args()
